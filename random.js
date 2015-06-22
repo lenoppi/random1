d0069e299506fdd26861c1ab841b4b6cb260b289
lenoppi <lenore.moppi@gmail.com>
2015-06-22 17:57:26 +0200

    version 2

--- random.js
+++ random.js
@@ -4,7 +4,10 @@ var numero = Math.random();
 var str =' MAYOR que 0,5';
 
 if (numero <= 0.5){
-  str = ' MENOR que 0,5';
+  console.log('\n'+numero+' Menor que 0,5 \n');
+}
+else {
+	console.log('\n'+numero+' Mayor que 0,5 \n');
 }
 
 console.log('\n' + numero + str + '\n');
